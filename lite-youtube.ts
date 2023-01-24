2f964b7195461da1ad4cac515176462cf42b7b8a
Neil Italia <hello@neilitalia.dev>
2023-01-24 10:07:15 -0600

    add youtube logo styling

--- lite-youtube.ts
+++ lite-youtube.ts
@@ -162,19 +162,23 @@ export class LiteYTEmbed extends HTMLElement {
         }
 
         #playButton {
-          width: 70px;
-          height: 46px;
-          background-color: var(--lyt-play-btn-hover);
+          width: 68px;
+          height: 48px;
+          background-color: transparent;
+          background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 48"><path d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.63 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z" fill="red"/><path d="M45 24 27 14v20" fill="white"/></svg>');
+          filter: grayscale(100%);
           z-index: 1;
           opacity: 0.8;
           border-radius: 14%;
           transition: var(--lyt-animation);
           border: 0;
+          border-radius: inherit;
         }
 
         #frame:hover > #playButton {
-          background-color: var(--lyt-play-btn-hover);
-          opacity: 1;
+          background-color: transparent;
+          filter: none;
+          border-radius: inherit;
         }
 
         #playButton:before {
